af64e5dd35966e5f6ee7971df7c211010d71b182
Marwan Essam <marwanrabie11@gmail.com>
2024-02-23 05:15:51 +0200

    lab4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,25 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::controller(PostController::class)->prefix('/posts')->middleware('auth')->group(function () {
-
-    Route::get('/create', 'create')->name('posts.create');
-    Route::post('/', 'store')->name('posts.store');
-    Route::get('/', 'index')->name('posts.index');
-    Route::get('/{id}/edit', 'edit')->name('posts.edit');
-    Route::put('/{id}/update', 'update')->name('posts.update');
-    Route::delete('/{id}', 'destroy')->name('posts.destroy');
-});
-Route::controller(StudentController::class)->prefix('/students')->middleware('auth')->group(function () {
-
-    Route::get('/create', 'create')->name('students.create');
-    Route::post('/', 'store')->name('students.store');
-    Route::get('/', 'index')->name('students.index');
-    Route::get('/{id}/edit', 'edit')->name('students.edit');
-    Route::put('/{id}/update', 'update')->name('students.update');
-    Route::delete('/{id}', 'destroy')->name('students.destroy');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
